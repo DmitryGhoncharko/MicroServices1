c2da0e64be5dd67a93badaa8a804da105e41a7c5
dangeonMaster <dafae722@gmail.com>
2023-05-31 18:55:18 +0300

    Initial commit

---
+++ src/main/java/com/project/MicroServices/FirstService/MicroServicesApplication.java
@@ -0,0 +1,15 @@
+package com.project.MicroServices.FirstService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServicesApplication.class, args);
+    }
+}
+
+
+
+
