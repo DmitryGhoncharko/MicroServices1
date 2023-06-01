7d3c025cf6e7792dc5ebb6a13f15a8dd982f92da
dangeonMaster <dafae722@gmail.com>
2023-06-01 15:29:43 +0300

    ss

--- src/main/java/com/project/MicroServices/FirstService/MicroServicesApplication.java
+++ src/main/java/com/project/MicroServices/FirstService/MicroServicesApplication.java
@@ -3,10 +3,16 @@ package com.project.MicroServices.FirstService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collection;
+import java.util.Collections;
+
 @SpringBootApplication
 public class MicroServicesApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MicroServicesApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(MicroServicesApplication.class);
+        springApplication.setDefaultProperties(Collections.singletonMap("server.port","8082"));
+        springApplication.run(args);
+
     }
 }
 
